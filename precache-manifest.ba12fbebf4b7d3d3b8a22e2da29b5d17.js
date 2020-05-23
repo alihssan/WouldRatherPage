9a1835cae6336f5581943f62860f24d76d5893ab
Ali Hassan <alyhassan862@gmail.com>
2020-05-23 18:21:57 +0000

    Updates

---
+++ precache-manifest.ba12fbebf4b7d3d3b8a22e2da29b5d17.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408edebf0229d64cf78270f84dc8df27",
+    "url": "/WouldRatherPage/index.html"
+  },
+  {
+    "revision": "f15e087f6950f437e939",
+    "url": "/WouldRatherPage/static/css/2.a1ff8c98.chunk.css"
+  },
+  {
+    "revision": "bff9b8f4789558750619",
+    "url": "/WouldRatherPage/static/css/main.750eb057.chunk.css"
+  },
+  {
+    "revision": "f15e087f6950f437e939",
+    "url": "/WouldRatherPage/static/js/2.02e3d07e.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/WouldRatherPage/static/js/2.02e3d07e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bff9b8f4789558750619",
+    "url": "/WouldRatherPage/static/js/main.1acdc716.chunk.js"
+  },
+  {
+    "revision": "867f67f98166f250082b",
+    "url": "/WouldRatherPage/static/js/runtime-main.972b2b0d.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/WouldRatherPage/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/WouldRatherPage/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/WouldRatherPage/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/WouldRatherPage/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/WouldRatherPage/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/WouldRatherPage/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/WouldRatherPage/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/WouldRatherPage/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/WouldRatherPage/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/WouldRatherPage/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/WouldRatherPage/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/WouldRatherPage/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/WouldRatherPage/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/WouldRatherPage/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/WouldRatherPage/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/WouldRatherPage/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
